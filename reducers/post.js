d226bbc5946299e1ef053ebad92c5231b2a3b7b3
ina <bbangina@gmail.com>
2021-01-15 15:11:36 +0900

    refactor:APPLY immer

--- reducers/post.js
+++ reducers/post.js
@@ -1,4 +1,5 @@
 import shortId from "shortid";
+import produce from "immer";
 
 export const initialState = {
   mainPosts: [
@@ -112,93 +113,73 @@ const dummyComment = (data) => ({
   User: { id: 1, nickname: "인아" },
 });
 
+//REDUCER? 이전 상태를 action을 통해 다음 상태로 만들어내는 함수(불변성은 지키면서)
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST_REQUEST:
-      return {
-        ...state,
-        isPostAdding: true,
-        isPostAdded: false,
-        postAddError: null,
-      };
-    case ADD_POST_SUCCESS:
-      return {
-        ...state,
-        //dummyPost를 spread 앞에다가 추가해야 위에 올라간다!!
-        mainPosts: [dummyPost(action.data), ...state.mainPosts],
-        isPostAdded: true,
-        isPostAdding: false,
-        postAddError: null,
-      };
-    case ADD_POST_FAILURE:
-      return {
-        ...state,
-        isPostAdded: false,
-        isPostAdding: false,
-        postAddError: action.error,
-      };
-    case REMOVE_POST_REQUEST:
-      return {
-        ...state,
-        isPostRemoving: true,
-        isPostRemoved: false,
-        postRemoveError: null,
-      };
-    case REMOVE_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: state.mainPosts.filter((v) => v.id !== action.data),
-        isPostRemoved: true,
-        isPostRemoving: false,
-        postRemoveError: null,
-      };
-    case REMOVE_POST_FAILURE:
-      return {
-        ...state,
-        isPostRemoved: false,
-        isPostRemoving: false,
-        postRemoveError: action.error,
-      };
-    case ADD_COMMENT_REQUEST:
-      return {
-        ...state,
-        isCommentAdding: true,
-        isCommentAdded: false,
-        commentAddError: null,
-      };
-    case ADD_COMMENT_SUCCESS: {
-      //불변성을 지키며 comment 추가하기!!!!!!!!
-      //(기존 객체들의 "참조는 유지"하되, 새로 추가되는 애만 생성하는 것)
-      //1. comment의 부모 post의 id를 알아낸다
-      const postIndex = state.mainPosts.findIndex(
-        (p) => p.id === action.data.postId
-      );
-      //2. 알아낸 id로 해당 post 데려온다.
-      const post = state.mainPosts[postIndex];
-      //3. dummyComment와 해당 post의 댓글들을 얕은 복사하여 합쳐서 저장한다.
-      const Comments = [dummyComment(action.data.content), ...post.Comments];
-      //4. mainPost 얕은 복사
-      const mainPosts = [...state.mainPosts];
-      mainPosts[postIndex] = { ...post, Comments };
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case ADD_POST_REQUEST:
+        draft.isPostAdding = true;
+        draft.isPostAdded = false;
+        draft.postAddError = null;
+        break;
 
-      return {
-        ...state,
-        mainPosts,
-        isCommentAdded: true,
-        isCommentAdding: false,
-        commentAddError: null,
-      };
+      case ADD_POST_SUCCESS:
+        draft.isPostAdded = true;
+        draft.isPostAdding = false;
+        draft.postAddError = null;
+        draft.mainPosts.unshift(dummyPost(action.data)); //w/o immer => [dummyPost(action.data), ...state.mainPosts]
+        break;
+
+      case ADD_POST_FAILURE:
+        draft.isPostAdded = false;
+        draft.isPostAdding = false;
+        draft.postAddError = action.error;
+        break;
+
+      case REMOVE_POST_REQUEST:
+        draft.isPostRemoving = true;
+        draft.isPostRemoved = false;
+        draft.postRemoveError = null;
+        break;
+
+      case REMOVE_POST_SUCCESS:
+        draft.mainPosts = state.mainPosts.filter((v) => v.id !== action.data);
+        draft.isPostRemoved = true;
+        draft.isPostRemoving = false;
+        draft.postRemoveError = null;
+        break;
+
+      case REMOVE_POST_FAILURE:
+        draft.isPostRemoved = false;
+        draft.isPostRemoving = false;
+        draft.postRemoveError = action.error;
+        break;
+
+      case ADD_COMMENT_REQUEST:
+        draft.isCommentAdding = true;
+        draft.isCommentAdded = false;
+        draft.commentAddError = null;
+
+      case ADD_COMMENT_SUCCESS: {
+        const post = draft.mainPosts.find((v) => v.id === action.data.postId);
+        post.Comments.unshift(dummyComment(action.data.content));
+
+        draft.isCommentAdded = true;
+        draft.isCommentAdding = false;
+        draft.commentAddError = null;
+
+        break;
+      }
+
+      case ADD_COMMENT_FAILURE:
+        draft.isCommentAdded = false;
+        draft.isCommentAdding = false;
+        draft.commentAddError = action.error;
+        break;
+
+      default:
+        return state;
     }
-    case ADD_COMMENT_FAILURE:
-      return {
-        ...state,
-        isCommentAdded: false,
-        isCommentAdding: false,
-        commentAddError: action.error,
-      };
-
-    default:
-      return state;
-  }
+  });
 };
 export default reducer;
